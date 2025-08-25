e54a9a56fdaae7504ba65cc82d208382befb2126
rolyan66@gmail.com <rolyan66@gmail.com>
2025-08-25 14:06:41 -0400

    q:

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { Router } from '@angular/router';
-=======
->>>>>>> ba9c2cf707c0f5a698bad1e75e2475b63e150508
 
 @Component({
   selector: 'app-inicio',
@@ -14,10 +11,7 @@ edad=25;
 peso=70;
 altura=170;
 sexo='masculino';
-<<<<<<< HEAD
 constructor(private router:Router){}
-=======
->>>>>>> ba9c2cf707c0f5a698bad1e75e2475b63e150508
 cambiarAltura(event:any){
   this.altura=event.target.value;
 }
@@ -27,11 +21,8 @@ masculino(){
 femenino(){
   this.sexo='femenino';
 }
-<<<<<<< HEAD
 calcularBMI(){
   this.router.navigate(['/resultado'])
 }
 }
-=======
-}
->>>>>>> ba9c2cf707c0f5a698bad1e75e2475b63e150508
+
