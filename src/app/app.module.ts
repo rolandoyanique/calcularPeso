f18a164a76872e069f5d0168bc187f537e5ebd3b
rolyan66@gmail.com <rolyan66@gmail.com>
2025-08-27 08:46:00 -0400

    Se adiciono el router para la navegacion entre paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//import { AppRoutingModule } from './app-routing.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
